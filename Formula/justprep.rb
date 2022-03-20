d0f23f5d06299b9d1fdb42517b358aaeaaf4ecf7
Greg Lutostanski <glutostanski@auntbertha.com>
2022-03-20 15:42:22 -0500

    justprep 1.0.2 (new formula)
    * first stanza
    * Some further changes
    * fixup style
    * fix description style
    * add subdir
    * use mkdir as style complains
    * yes more style
    * try with just
    * Add pcre runtime dep
    * More runtime library deps
    
    Closes #1.
    
    Co-authored-by: Greg Lutostanski <greg.lutostanski@mobilityhouse.com>
    Signed-off-by: Greg Lutostanski <greg.luto@gmail.com>

---
+++ Formula/justprep.rb
@@ -0,0 +1,23 @@
+class Justprep < Formula
+  desc "Pre-processor for the just command-line utility"
+  homepage "https://github.com/MadBomber/justprep"
+  url "https://github.com/MadBomber/justprep/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "7b482e617d562afb277565f2fc5f331ea0a1d72e548730a19e9117f58852e5df"
+  license "MIT"
+
+  depends_on "crystal" => :build
+  depends_on "just" => :build
+
+  depends_on "bdw-gc"
+  depends_on "libevent"
+  depends_on "pcre"
+
+  def install
+    system "just", "static=true", "crystal/build"
+    bin.install "./crystal/bin/justprep"
+  end
+
+  test do
+    system "#{bin}/justprep", "--version"
+  end
+end
