8187988ac99641df6c06f89d50684a2294529b57
Greg Lutostanski <greg.lutostanski@mobilityhouse.com>
2022-03-20 16:03:41 -0500

    fix description style

--- Formula/justprep.rb
+++ Formula/justprep.rb
@@ -1,5 +1,5 @@
 class Justprep < Formula
-  desc "pre-processor for the just command-line utility"
+  desc "Pre-processor for the just command-line utility"
   homepage "https://github.com/MadBomber/justprep"
   url "https://github.com/MadBomber/justprep/archive/refs/tags/v1.0.2.tar.gz"
   sha256 "7b482e617d562afb277565f2fc5f331ea0a1d72e548730a19e9117f58852e5df"
