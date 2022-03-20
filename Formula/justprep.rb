540b88e0e6938a08b567fac4d615efa79edee93d
Greg Lutostanski <glutostanski@auntbertha.com>
2022-03-20 15:42:22 -0500

    first stanza

---
+++ Formula/justprep.rb
@@ -0,0 +1,8 @@
+class Justprep < Formula
+  desc "A pre-process for the just command line utility"
+  homepage "https://github.com/MadBomber/justprep"
+  url "https://github.com/MadBomber/justprep/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 ""
+  license "MIT"
+
+end
